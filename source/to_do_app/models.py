9349e3d35eab1c82b982ce5c1976317c6bcf0840
Aigerim Avezbaeva <av21m1999@gmail.com>
2023-02-14 01:31:29 +0600

    добавлена модель

--- source/to_do_app/models.py
+++ source/to_do_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class ToDoParagraph(models.Model):
+    STATUS_CHOICES = {
+        ('new', 'Новая'),
+        ('in_process', 'В процессе'),
+        ('complete', 'Завершена')
+    }
+    description = models.CharField(max_length=400, null=False, verbose_name='Задача')
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='new', verbose_name='Статус задачи')
+    completion_date = models.DateField(verbose_name='Дата выполнения', null=True)
+
+    @classmethod
+    def return_choices(cls):
+        return cls.STATUS_CHOICES
+
