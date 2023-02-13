334db91d28ef1687d4bc343470e4670d0bbb59e6
Aigerim Avezbaeva <av21m1999@gmail.com>
2023-02-14 01:58:49 +0600

    добавила админку

--- source/to_do_app/admin.py
+++ source/to_do_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from source.to_do_app.models import ToDoParagraph
+
+
 # Register your models here.
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'status')
+    list_filter = ('id', 'title', 'status', 'completion_date')
+    search_fields = ('title', 'status')
+    fields = ('id', 'title', 'status', 'completion_date')
+    readonly_fields = ('id', 'title')
+
+
+admin.site.register(ToDoParagraph, TodoAdmin)
