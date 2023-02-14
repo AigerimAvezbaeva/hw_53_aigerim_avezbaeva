7968c2b7b6881a838ad77a9a0005940c89c62d5c
Aigerim Avezbaeva <av21m1999@gmail.com>
2023-02-14 14:11:20 +0600

    корректировки ошибок сс принтом

--- source/to_do_app/views/base.py
+++ source/to_do_app/views/base.py
@@ -6,9 +6,7 @@ from to_do_app.models import ToDoParagraph
 
 def to_do_view(request: WSGIRequest):
     paragraphs = ToDoParagraph.objects.all()
-    print(paragraphs)
     context = {
         'paragraphs': paragraphs
     }
-    print(context)
     return render(request, 'to_do_index.html', context=context)
