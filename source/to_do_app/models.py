5018908be2c4515849d2a0a3db3c7ce57886329b
Aigerim Avezbaeva <av21m1999@gmail.com>
2023-02-14 07:04:51 +0600

    добавила стили

--- source/to_do_app/models.py
+++ source/to_do_app/models.py
@@ -3,16 +3,20 @@ from django.db import models
 
 # Create your models here.
 class ToDoParagraph(models.Model):
-    STATUS_CHOICES = {
+    STATUS_CHOICES = (
         ('new', 'Новая'),
         ('in_process', 'В процессе'),
         ('complete', 'Завершена')
-    }
-    title = models.CharField(max_length=400, blank=True, null=False, verbose_name='Задача')
-    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='new', verbose_name='Статус задачи')
-    completion_date = models.DateField(verbose_name='Дата выполнения', null=True)
+    )
+    title = models.CharField(max_length=400, blank=False, null=False, verbose_name='Задача')
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, blank=False,  default='new', verbose_name='Статус задачи')
+    completion_date = models.DateField(verbose_name='Дата выполнения', blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.title} - {self.status}'
 
     @classmethod
     def return_choices(cls):
-        return cls.STATUS_CHOICES
+        statuses = dict(cls.STATUS_CHOICES)
+        return statuses
 
